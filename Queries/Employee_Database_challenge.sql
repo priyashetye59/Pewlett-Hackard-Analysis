86628f2467c3fe1492fa97c211c37d68166752e2
priyashetye59 <108565320+priyashetye59@users.noreply.github.com>
2022-08-18 21:01:08 -0400

    Saving deliverable 1 of assignment

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,35 @@
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
 
+SELECT * FROM retirement_titles
+
+-- Retirement employees with unique titles
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles
+
+--Unique retirement employees title count
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles
